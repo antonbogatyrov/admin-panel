05d62a2f21a62d3a13c3d40133a4701e0f18bb83
Anton Bogatyrov <antonbogatyrov@Antons-MacBook-Pro.local>
2018-01-15 12:25:15 +0200

    init eslsint

--- README.md
+++ README.md
@@ -18,5 +18,5 @@
 ## Check code
 
 ```sh
- yarn lint
+ yarn eslint
 ```
--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,18 +1,9 @@
 const webpack = require('webpack');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   devtool: 'source-map',
   module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextWebpackPlugin.extract({
-          use: [{ loader: 'css-loader', options: { minimize: true } }, 'sass-loader'],
-          fallback: 'style-loader',
-        }),
-      },
-    ],
+    rules: [],
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
