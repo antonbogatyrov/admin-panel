2a3fcbf86fcc0e8cc6718876ffe51e8b5ecd1020
Anton Bogatyrov <antonbogatyrov@Antons-MacBook-Pro.local>
2018-01-21 17:09:04 +0200

    add compose to base

--- src/container/Base/index.js
+++ src/container/Base/index.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux';
 import { withStyles } from 'material-ui/styles';
+import { compose } from 'redux';
 
 import Component from './Component';
 import { mapStateToProps, mapDispatchToFunc } from './controller';
 import style from './style';
 
-export default withStyles(style)(connect(mapStateToProps, mapDispatchToFunc)(Component));
+export default compose(
+  withStyles(style),
+  connect(mapStateToProps, mapDispatchToFunc),
+)(Component);
+
